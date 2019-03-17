fc4b8aa6d8977fc8f3def797cb35abae5a983288
Jason Dexter <jddexter@ucdavis.edu>
2019-03-16 20:31:44 -0700

    added code to data preprocessing

--- Part 1 - Data Preprocessing/data_preprocessing_template.R
+++ Part 1 - Data Preprocessing/data_preprocessing_template.R
@@ -1,16 +1,88 @@
 # Data Preprocessing Template
 
+# 1.0 Setup ----
+
+# Load libraries
+library(tidyverse)
+library(rsample)
+
 # Importing the dataset
-dataset = read.csv('Data.csv')
+dataset_raw_tbl = read_csv('Data.csv')
+
+
+# 2.0 Preprocess Data ----
+
+
+dataset_tbl <- dataset_raw_tbl %>% 
+    
+    # Replace rows w/missing age with avg. age
+    mutate(Age = if_else(is.na(Age),
+                    mean(Age, na.rm = T),
+                    Age)) %>% 
+    
+    # Replace rows w/missing salary with avg. salary
+    mutate(Salary = if_else(is.na(Salary),
+                        mean(Salary, na.rm = T),
+                        Salary)) %>% 
+    
+    # Encode categorical by converting to factor (forcats library way)
+    mutate(Country_Fct   = factor(Country,
+                                  levels = c("France", "Spain", "Germany"),
+                                  labels = c(1, 2, 3))) %>% 
+    
+    # Encode Using factor() to set labels 0 & 1 (base R way)
+    mutate(Purchased_Fct = factor(Purchased,
+                                 levels = c("No", "Yes"),
+                                 labels = c(0, 1))) %>% 
+    
+    # Reorder columns
+    select(Age, Salary, contains("Cou"), contains("Pur"))
+
+dataset_tbl
+
+
+# 3.0 Split Data ----
 
 # Splitting the dataset into the Training set and Test set
-# install.packages('caTools')
-library(caTools)
 set.seed(123)
-split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
-training_set = subset(dataset, split == TRUE)
-test_set = subset(dataset, split == FALSE)
+split_obj <- initial_split(data = dataset_tbl,
+                           prop = 0.7,
+                           strata = "Purchased")
 
-# Feature Scaling
+train_tbl <- split_obj %>% training()
+test_tbl  <- split_obj %>% testing()
+
+
+# 4.0 Feature Scaling ----
+
+# Puts numerical features on the same scale
+# This is done so that higher values in one feature, do not dominate small 
+    # values in another feature (being squared in pythagorean for euclidian distance)
+# Transforms variables so that they are in the same range, same scale so that
+    # no varible is dominated by the other.
+
+# Could do it this way with base R: Leaves weird output for column name
+train_tbl %>% 
+    
+    mutate(Age_Scaled    = scale(Age),
+           Salary_Scaled = scale(Salary))
+
+# A better way with Recipes (can do entire preprocessing step pipeline)
+
+# Link to Recipes: https://tidymodels.github.io/recipes/
+# Simple example:  https://tidymodels.github.io/recipes/articles/Simple_Example.html
+
+library(recipes)
+
+rec_obj <- recipe()
+
+
+
+
 # training_set = scale(training_set)
 # test_set = scale(test_set)
+
+
+
+
+
